2c753d991d7d09734db96ca92bd6c3f9f2350d97
Rohit Patil <61861303+rohit-raje-786@users.noreply.github.com>
2021-07-14 13:15:13 +0530

    resume link updated

--- src/profile.js
+++ src/profile.js
@@ -174,7 +174,7 @@ const social = {
   linkedin: "https://www.linkedin.com/in/rohit-patil-576216192",
 
   resume:
-    "https://docs.google.com/document/d/1Df5AghehdbS-HhrRbLWGtHb8FNrnROi3P2p6O0Y07Hg/edit?usp=sharing",
+    "https://drive.google.com/file/d/160iu5j3iLmaVM6dxEBxP0WrsiqaUoFpL/view?usp=sharing",
 };
 // Dont change anything here
 export {
