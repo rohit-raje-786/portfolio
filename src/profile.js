c6570a766ea43615d32d991f0e361161fffae2c9
rohit-raje-786 <rp589006@gmail.com>
2022-07-27 00:40:18 +0530

    about changed

--- src/profile.js
+++ src/profile.js
@@ -20,7 +20,7 @@ const background = {
 const section2title = "About Me";
 const about = {
   paragraph:
-    "I am a Self taught Full Stack Web Developer,currently pursuing my B-TECH from KJ Somaiya College Of Engineering.In the past I Intern at Arconn as a software developer.Currently I am Interning at Xoog as an App Developer and focusing on DSA.During my free time I like to solve problems on Leetcode,GFG which helps me to brush up my coding skills.I am a keen learner and always like to explore new technology and stack.",
+    "I am a Self taught Full Stack Web Developer,currently pursuing B-TECH from KJ Somaiya College Of Engineering majors in Information Technology.In am MLH Prep Fellow and App Developer intern at XOOG.During my free time I like to solve problems on Leetcode,GFG which helps me to brush up my coding skills.I am a keen learner and always like to explore new technology and stack.",
   codechef: "https://www.codechef.com/users/rohitraje_123",
   hackerRank: "https://www.hackerrank.com/rp589006",
   leetcode: "https://leetcode.com/rohitraje/",
